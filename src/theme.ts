ccbb2508fefa3d805cd4f505ad2c871b25597b5a
Jojo12End <misajonathan0804@gmail.com>
2026-04-08 11:20:05 +0200

    feat: add src/theme.ts

---
+++ src/theme.ts
@@ -0,0 +1,63 @@
+export const Theme = {
+  colors: {
+    background: '#060e20',
+    surface: '#060e20',
+    surface_container_lowest: '#000000',
+    surface_container_low: '#081329',
+    surface_container: '#0c1934',
+    surface_container_high: '#101e3e',
+    surface_container_highest: '#142449',
+    surface_bright: '#172b54',
+    
+    primary: '#919bff',
+    primary_container: '#818cf8',
+    on_primary: '#000b83',
+    on_primary_container: '#00055a',
+    
+    secondary: '#7bd0ff',
+    secondary_container: '#00293b',
+    on_secondary: '#004560',
+    on_secondary_container: '#1dafea',
+    
+    tertiary: '#ffafd3',
+    tertiary_container: '#ff99ca',
+    on_tertiary: '#7c0953',
+    
+    error: '#fd6f85',
+    error_container: '#8a1632',
+    on_error: '#490013',
+    on_error_container: '#ff97a3',
+    
+    outline: '#65759e',
+    outline_variant: '#38476d',
+    
+    on_surface: '#dee5ff',
+    on_surface_variant: '#9baad6',
+    on_background: '#dee5ff',
+  },
+  typography: {
+    fontFamily: {
+      headline: 'PlusJakartaSans_700Bold',
+      headlineMedium: 'PlusJakartaSans_600SemiBold',
+      body: 'Manrope_400Regular',
+      bodyMedium: 'Manrope_500Medium',
+      bodyBold: 'Manrope_700Bold',
+      label: 'Manrope_600SemiBold',
+    },
+  },
+  spacing: {
+    xs: 4,
+    sm: 8,
+    md: 16,
+    lg: 24,
+    xl: 32,
+    xxl: 48,
+  },
+  radius: {
+    sm: 4,
+    md: 12,
+    lg: 16,
+    xl: 24,
+    full: 9999,
+  }
+};
